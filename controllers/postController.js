3a7f9400f4b60f174ecdb6d3b48c1ed5438e5988
dutaramadana <gungdeduta@gmail.com>
2023-06-07 15:45:52 +0800

    refactoring get all post and add createPost

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,22 +6,93 @@ import { User, Post } from "../models/UserPostAssociations.js";
  */
 const getAllPosts = async (req, res, next) => {
   try {
+    // variabel dalam url berupa string, kita harus mengkonversi menjadi int
+    const page = parseInt(req.query.page) || 0;
+    const limit = parseInt(req.query.limit) || 5;
+    const author = req.query.author || "";
+    const title = req.query.title || "";
+    const offset = limit * page;
 
-    const result = await Post.findAll({
-      include: [{
-          model: User,
+    const totalRows = await Post.count({
+      where: {
+        title: {
+          [Op.like]: `%${title}%`,
         },
-      ],
+      },
+      include: {
+        model: User,
+        attributes: [`username`, `email`],
+        where: {
+          username: {
+            [Op.like]: `%${author}%`,
+          },
+        },
+      },
     });
 
-    res.status(200).json({ result });
+    const totalPage = Math.ceil(totalRows / limit);
 
+    const result = await Post.findAll({
+      where: {
+        title: {
+          [Op.like]: `%${title}%`,
+        },
+      },
+      include: {
+        model: User,
+        attributes: [`username`, `email`],
+        where: {
+          username: {
+            [Op.like]: `%${author}%`,
+          },
+        },
+      },
+      offset,
+      limit,
+    });
+
+    let data = [];
+    result.forEach((currentValue, index) => {
+      data.push({
+        id: currentValue.id,
+        title: currentValue.title,
+        slug: currentValue.slug,
+        content: currentValue.content,
+        createdAt: currentValue.createdAt,
+        updatedAt: currentValue.updatedAt,
+        author: currentValue.user.username,
+      });
+    });
+
+    res.status(200).json({
+      data,
+      page,
+      limit,
+      totalRows,
+      totalPage,
+      totalRows,
+    });
   } catch (error) {
-
     console.log(error);
     res.status(500).json({ error });
-    
   }
 };
 
-export { getAllPosts };
+const createPost = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { title, content } = req.body;
+    const slug = title
+      .replace(/[^\w\s]/gi, "")
+      .replace(/\s+/g, "-")
+      .toLowerCase();
+
+    const post = await Post.create({ title, slug, content, userId });
+
+    res.status(201).json({ status: "CREATED", data: post });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export { getAllPosts, createPost };
